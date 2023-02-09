24c1b1cf09bb79009875f60dd8066432bc1a80b9
Jay <jayadityanautiyal07@gmail.com>
2023-02-09 20:57:42 +0530

    resolved initial issue

--- pages/api/logReport.js
+++ pages/api/logReport.js
@@ -1,12 +1,21 @@
 import { MongoClient } from 'mongodb';
+import { ChatGPTAPI } from 'chatgpt'
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const client = new MongoClient('mongodb+srv://demodb:demodb@metaverse.ofmwexd.mongodb.net/?retryWrites=true&w=majority');
     await client.connect();
     console.log('Connected to mongo');
     const db = client.db()
-    const collection = db.collection("reports")
-    await collection.insertOne(req.body);
+    const collection = db.collection("reports");
+
+    const api = new ChatGPTAPI({
+      apiKey: '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'
+    })
+    // console.log(req.body)
+    // console.log(JSON.stringify(req.body.head_commit))
+    const resp = await api.sendMessage(`Tell  all the keys of this stringified JSON : ${JSON.stringify(req.body.head_commit)}`)
+    console.log("chat gpt output", resp.text)
+    await collection.insertOne({output : resp.text});
     client.close()
     res.status(200).json({ msg: 'event logged' });
   } else res.status(405).json({msg: 'Invalid request method'});
