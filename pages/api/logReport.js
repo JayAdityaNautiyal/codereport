316e2b9bc1eefb72a7bbd43c7b54f9886b5bb347
Jay <jayadityanautiyal07@gmail.com>
2023-02-09 17:22:04 +0530

    Added mongo client

--- pages/api/logReport.js
+++ pages/api/logReport.js
@@ -1,6 +1,13 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb';
+export default async function handler(req, res) {
   if (req.method === 'POST') {
-    //fs.writeFileSync('public/events.json', JSON.stringify(req.body));
+    const client = new MongoClient('mongodb+srv://demodb:demodb@metaverse.ofmwexd.mongodb.net/?retryWrites=true&w=majority');
+    await client.connect();
+    console.log('Connected to mongo');
+    const db = client.db()
+    const collection = db.collection("reports")
+    await collection.insertOne(req.body);
+    client.close()
     res.status(200).json({ msg: 'event logged' });
   } else res.status(405).json({msg: 'Invalid request method'});
 }
