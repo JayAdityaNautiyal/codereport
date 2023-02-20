607025f0c7e56851ef1bde308095be2b7c4381c3
Jay <jayadityanautiyal07@gmail.com>
2023-02-20 13:37:36 +0530

    file with defects added

--- pages/api/logReport.js
+++ pages/api/logReport.js
@@ -1,5 +1,4 @@
 import { MongoClient } from "mongodb";
-// import { ChatGPTAPI } from "chatgpt";
 import { Configuration, OpenAIApi } from "openai"
 import axios from "axios";
 
@@ -21,27 +20,11 @@ export default async function handler(req, res) {
 
     for (const filepath of req.body.head_commit.modified) {
       const url = `https://raw.githubusercontent.com/${req.body.repository.owner.name}/${req.body.repository.name}/main/${filepath}`;
+      console.log("url ", url)
       const response = await axios.get(url);
-
+      console.log("response ", response)
       const content = response.data;
       const prompt = `find the errors in the code, explain the errors and give the corrected code - ${content}`;
-      console.log("content= ", content, typeof content);
-      // const decodedContent = Buffer.from(content, "base64").toString("utf-8");
-      // console.log("decoded content = ", decodedContent)
-      // const resp = await api.sendMessage(
-      //   `;- ${content}`
-      // );
-      // console.log("chat gpt output", resp.text);
-      // await collection.insertOne({ output: resp.text });
-      // const api = new ChatGPTAPI({
-      //   apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
-      //   completionParams: { model:"text-davinci-003", prompt:prompt , max_tokens:3600}
-      // });
-
-      
-
-    
-
       const completion = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: prompt,
@@ -50,10 +33,6 @@ export default async function handler(req, res) {
       console.log(completion.data.choices[0].text);
       await collection.insertOne({ output: completion.data.choices[0].text });
     }
-
-    // console.log(req.body)
-    // console.log(JSON.stringify(req.body.head_commit))
-
     client.close();
     res.status(200).json({ msg: "event logged" });
   } else res.status(405).json({ msg: "Invalid request method" });
